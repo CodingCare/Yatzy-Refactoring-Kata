c5aaba01d750aeaad0d30a84227adc2ae356c06e
Arnaud Brunet <gronono@users.noreply.github.com>
2021-02-25 20:27:04 +1100

    final solution

--- src/main/java/yatzy/DiceRoll.java
+++ src/main/java/yatzy/DiceRoll.java
@@ -1,29 +1,26 @@
-import java.util.Arrays;
+package yatzy;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
+import static java.util.Comparator.reverseOrder;
 import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class DiceRoll {
-    /**
-     * Same as {@link Collectors#counting()} but returns an Integer instead of a Long.
-     */
-    // FIXME Rien à faire ici
-    private static final Collector<Integer, ?, Integer> countingInt = reducing(0, (previous, current) -> previous + 1);
 
     private final List<Integer> dice;
 
     public DiceRoll(int d1, int d2, int d3, int d4, int d5) {
-        this.dice = Arrays.asList(d1, d2, d3, d4, d5);
+        this.dice = asList(d1, d2, d3, d4, d5);
     }
 
     public Map<Integer, Integer> counts() {
-        return dice.stream().collect(groupingBy(identity(), countingInt));
+        return dice.stream().collect(groupingBy(identity(), YatzyCollectors.countingInt));
     }
 
     public int countDice(int dice) {
@@ -48,12 +45,33 @@ public class DiceRoll {
     }
 
     public List<Integer> findPairs() {
-        return filterNumberOfDiceGreaterThan(2).collect(toList());
+        return filterNumberOfDiceGreaterThan(2)
+                .sorted(reverseOrder())
+                .collect(toList());
     }
 
     public int getDiceWithCountGreaterThan(int n) {
         return filterNumberOfDiceGreaterThan(n)
                 .findFirst()
                 .orElse(0);
+    }
+
+    private List<Integer> sort() {
+        return dice.stream().sorted().collect(toList());
+    }
+
+    public boolean isSmallStraight() {
+        return sort().equals(asList(1, 2, 3, 4, 5));
+    }
+
+    public boolean isLargeStraight() {
+        return sort().equals(asList(2, 3, 4, 5, 6));
+    }
+
+    public boolean isFullHouse() {
+        boolean hasThreeOfAKind = getDiceWithCountGreaterThan(3) != 0;
+        boolean hasPair = !findPairs().isEmpty();
+        boolean isYatzy = isYatzy();
+        return hasThreeOfAKind && hasPair && !isYatzy;
     }
 }
--- src/main/java/yatzy/YatzyScores.java
+++ src/main/java/yatzy/YatzyScores.java
@@ -1,8 +1,8 @@
+package yatzy;
+
 import java.util.List;
 
-import static java.util.Comparator.naturalOrder;
-
-public class Yatzy {
+/* package */ class YatzyScores {
 
     public static int chance(DiceRoll roll) {
         return roll.sum();
@@ -40,11 +40,12 @@ public class Yatzy {
     }
 
     public static int pair(DiceRoll roll) {
-        return roll.findPairs()
-                .stream()
-                .max(naturalOrder())
-                .map(dice -> dice * 2)
-                .orElse(0);
+        List<Integer> pairs = roll.findPairs();
+        if (pairs.isEmpty()) {
+            return 0;
+        }  else {
+            return pairs.get(0) * 2;
+        }
     }
 
     public static int twoPairs(DiceRoll roll) {
@@ -65,45 +66,22 @@ public class Yatzy {
         return roll.getDiceWithCountGreaterThan(4) * 4;
     }
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[0] == 1 &&
-                tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1)
+    public static int smallStraight(DiceRoll roll) {
+        if (roll.isSmallStraight()) {
             return 15;
+        }
         return 0;
     }
 
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1
-                && tallies[5] == 1)
+    public static int largeStraight(DiceRoll roll) {
+        if (roll.isLargeStraight()) {
             return 20;
+        }
         return 0;
     }
 
     public static int fullHouse(DiceRoll roll) {
-        boolean hasThreeOfAKind = roll.getDiceWithCountGreaterThan(3) != 0;
-        boolean hasPair = !roll.findPairs().isEmpty();
-        boolean isYatzy = roll.isYatzy();
-        if (hasThreeOfAKind && hasPair && !isYatzy) {
+        if (roll.isFullHouse()) {
             return roll.sum();
         }
         return 0;
